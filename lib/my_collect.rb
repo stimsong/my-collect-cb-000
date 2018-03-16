e9160aaff5699f47e8ef5e6ee838942786998ea2
Greg Stimson <stimsong@yahoo.com>
2018-03-16 20:50:34 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(array)
 
+  if block_given?
+    i = 0
+    collection = []
+
+    while i < array.length
+      collection << yield(array[i])
+      i += 1
+    end
+    collection
+  else
+    puts "No data"
+  end
+end
